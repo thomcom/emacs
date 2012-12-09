62cd2675d59b52ffb426b156a41404f5d8c89945
it3ration <it3ration>
2012-12-08 23:36:34 -0700

    removed auto mode for json files as it is enabled by default in emacs 24

--- .emacs.d/mode-mappings.el
+++ .emacs.d/mode-mappings.el
@@ -3,7 +3,4 @@
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
-;; json
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
-
 (provide 'mode-mappings)
