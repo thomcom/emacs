7f24d156e836130245e4e2b6c151f53c0e43d3d0
Kevin Depue <iwannaseethelight@gmail.com>
2012-12-12 11:23:31 -0700

    forced header files to be opened in c++ mode by default

--- .emacs.d/mode-mappings.el
+++ .emacs.d/mode-mappings.el
@@ -3,4 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
+;; header files
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
 (provide 'mode-mappings)
